6cadb0a799f561fa81cb61a2784ab44501517d46
Samuel Alvarez Alban <115104098+Freddyedd21@users.noreply.github.com>
2025-02-25 21:44:12 -0500

    upload parcial frontend

--- src/main/java/com/example/demo/VueJsExampleApplication.java
+++ src/main/java/com/example/demo/VueJsExampleApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class VueJsExampleApplication {
+public class
+VueJsExampleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VueJsExampleApplication.class, args);
